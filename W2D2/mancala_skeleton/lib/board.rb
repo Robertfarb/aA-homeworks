e3566f1b8b46692b36628b69018a56641736ac41
Robert Farb <farb.robert@gmail.com>
2018-06-25 21:44:08 -0700

    Add initialize, place_stoens, valid_move methods to board class

--- W2D2/mancala_skeleton/lib/board.rb
+++ W2D2/mancala_skeleton/lib/board.rb
@@ -1,17 +1,26 @@
 class Board
   attr_accessor :cups
 
-  def initialize(name1, name2)
+  def initialize(name1 = "bob", name2 = "simon")
+    @cups = Array.new(14) {[]}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      4.times {cup << :stone} unless idx == 6 || idx == 13
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos > cups.length
+    raise "Starting cup is empty" if cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    cups[start_pos] = []
+    cups[start_pos]
   end
 
   def next_turn(ending_cup_idx)
@@ -31,4 +40,10 @@ class Board
 
   def winner
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  brd1 = Board.new
+  brd1.place_stones
+  p brd1.cups
 end
