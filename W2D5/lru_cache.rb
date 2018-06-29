959a87e726835013014d07344c3f41ad56946643
Robert Farb <farb.robert@gmail.com>
2018-06-28 23:14:23 -0700

    Add W2D5 hw

---
+++ W2D5/lru_cache.rb
@@ -0,0 +1,45 @@
+class LRUCache
+  def initialize(size)
+    @size = size
+    @cache = []
+  end
+
+  def count
+    # returns number of elements currently in cache
+    @cache.count
+  end
+
+  def add(el)
+    # adds element to cache according to LRU principle
+    if cache.include?(el)
+      cache.delete(el)
+      cache.push(el)
+    elsif count >= size
+      cache.shift
+      cache.push(el)
+    else
+      cache.push(el)
+    end
+  end
+
+  def show
+    # shows the items in the cache, with the LRU item first
+    p cache
+  end
+
+  private
+  # helper methods go here!
+  attr_reader :cache, :size
+
+end
+
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+# johnny_cache.add(-5)
+
+johnny_cache.show
