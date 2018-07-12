de02d18954aad9716bda2ac2b7e4ac83b9544464
Robert Farb <farb.robert@gmail.com>
2018-07-11 22:56:32 -0700

    Complete user validations

--- W4D3/AuthHomework/app/models/user.rb
+++ W4D3/AuthHomework/app/models/user.rb
@@ -11,14 +11,30 @@
 #
 
 class User < ApplicationRecord
-  validates :password_digest, length: {minimum: 5}, presence: true
+  attr_reader :password
+  validates :password_digest, presence {message: 'Password can\'t be blank'}
+  validates :password, length: {minimum: 6, allow_nil: true}
   validates :username, :session_token, presence: true
 
+  def self.find_by_credentials(username, password)
+    User.find_by(username: username)
+  end
+
+  def self.genereate_session_token
+  end
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
   def password_digest_presence
     errors[:password_digest] << "password can't be blank" << if password_digest.length == 0
+  end
+
+  def reset_session_token!
+  end
+
+  def ensure_session_token
   end
 end
--- W4D3/AuthHomework/db/schema.rb
+++ W4D3/AuthHomework/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711154112) do
+ActiveRecord::Schema.define(version: 20180711154803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20180711154112) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password", null: false
   end
 
 end
