9e9db78f794cee31faa2583241cb352ced93b707
Robert Farb <farb.robert@gmail.com>
2018-07-12 21:47:45 -0700

    Remove W4D5 working tree

--- W4D5/capybara_test/music/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :create, :new, :show]
-
-  resource :session, only: [:create, :destroy, :new]
-
-  root 'sessions#new'
-end
--- W4D5/capybara_test/music/db/migrate/20180712173036_add_index_to_email_sess_token.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToEmailSessToken < ActiveRecord::Migration[5.1]
-  def change
-    add_index :users, :email, unique: true
-    add_index :users, :session_token, unique: true
-  end
-end
