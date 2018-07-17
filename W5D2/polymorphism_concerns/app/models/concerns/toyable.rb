7858d46fddadbb3c8a78d55287fe9a2d9ef1eb7a
Robert Farb <farb.robert@gmail.com>
2018-07-16 23:18:50 -0700

    Complete Toyable Concern:

--- W5D2/polymorphism_concerns/app/models/concerns/toyable.rb
+++ W5D2/polymorphism_concerns/app/models/concerns/toyable.rb
@@ -2,10 +2,12 @@ module Toyable
   extend ActiveSupport::Concern
 
   included do
-    
+    has_many :toys, as: :toyable
   end
 
   def receive_toy(name)
-
+    result_toy = Toy.find_or_create_by(name: name)
+    self.toys.concat(result_toy)
+    self.save
   end
 end
