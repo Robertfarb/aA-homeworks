c63b8b66206b4110da2437b06e297944461d5049
Robert Farb <farb.robert@gmail.com>
2018-07-20 08:54:36 -0700

    Complete tea and biscuits

--- W5D5/callbacks_functions.js
+++ W5D5/callbacks_functions.js
@@ -1,4 +1,32 @@
-window.setTimeout(function(){
-  alert('HAMMERTIME')
-}, 5000);
+// window.setTimeout(function(){
+//   alert('HAMMERTIME')
+// }, 5000);
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits() {
+  reader.question('Would you like some tea?', (res) => {
+    first = res;
+    console.log(`You replied ${res}.`);
+    
+    reader.question(`Would you like some biscuits?`, (res) => {
+      second = res;
+      console.log(`You replied ${res}`)
+
+      const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+      const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+      reader.close();
+    });
+  });
+}
+
+teaAndBiscuits()
+
+
 
