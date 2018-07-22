358294c572d3219e8b6eb1357250d42c9966295f
Robert Farb <farb.robert@gmail.com>
2018-07-22 13:45:29 -0700

    Complete phase III canvas demo

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -6,9 +6,12 @@ document.addEventListener("DOMContentLoaded", function(){
   ctx.fillRect(10, 10, 500, 500);
 
   ctx.beginPath();
-  ctx.arc(10, 10, 25, 25);
-  ctx.strokeStyle("perriwinkle");
-  ctx.lineWidth("5");
+  ctx.arc(100, 80, 50, 0, 2 * Math.PI);
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 5;
   ctx.stroke();
+  ctx.lineWidth = 5;
+  ctx.fillStyle = "chartreuse";
+  ctx.fill();
 });
 
