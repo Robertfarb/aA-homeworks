1e0e6346832e417891520ecb7b7ed58a4e0b73c8
Robert Farb <farb.robert@gmail.com>
2018-07-22 13:48:13 -0700

    complete canvas_demo

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -13,5 +13,16 @@ document.addEventListener("DOMContentLoaded", function(){
   ctx.lineWidth = 5;
   ctx.fillStyle = "chartreuse";
   ctx.fill();
+
+  ctx.beginPath();
+  ctx.arc(225, 225, 50, 0, Math.PI * 2, true); // Outer circle
+  ctx.moveTo(110, 75);
+  ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+  ctx.moveTo(65, 65);
+  ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+  ctx.moveTo(95, 65);
+  ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+  ctx.stroke();
+
 });
 
