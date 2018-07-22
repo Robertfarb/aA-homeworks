e5ae05c091aa4936872d728939375e6a2e2fa6a4
Robert Farb <farb.robert@gmail.com>
2018-07-22 13:41:05 -0700

    Complete phase1 canvas_demo

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
+  let canvas = document.getElementById('mycanvas');
+  let ctx = canvas.getContext('2d');
 
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, 10, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(10, 10, 25, 25);
+  ctx.strokeStyle("perriwinkle");
+  ctx.lineWidth("5");
+  ctx.stroke();
 });
+
