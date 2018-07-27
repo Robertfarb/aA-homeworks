50aa147d6db266927909e00c9d37f4750de9d3d6
Robert Farb <farb.robert@gmail.com>
2018-07-27 08:54:06 -0700

    Setup and complete calcultor state

---
+++ W6D5/calculator/calculator.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Calculator extends React.Component{
+  constructor(props){
+    super(props);
+    //your code here
+    this.state = {
+      result: 0,
+      num1: "",
+      num2: ""
+    }
+  }
+
+  //your code here
+
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+        //your code will replace this
+      </div>
+    );
+  }
+}
+
+export default Calculator;
