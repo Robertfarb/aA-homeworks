a43fb246d89fd4e37d863cb18a0bdc50058aa66e
Robert Farb <farb.robert@gmail.com>
2018-08-01 22:57:11 -0700

    Add w7d4 working tree

--- W7D3/giphy_redux/giphy_search.jsx
+++ W7D3/giphy_redux/giphy_search.jsx
@@ -1,4 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
 import Root from './components/root';
+import configureStore from './store/store';
+import fetchSearchGiphys from './util/api_util';
+import { receiveSearchGiphys } from './actions/giphy_actions';
+import rootReducer from './reducers/root_reducer';
+
+const store = configureStore();
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("loaded!")
+  window.store = store;
+  window.fetchSearchGiphys = fetchSearchGiphys;
+  window.receiveSearchGiphys = receiveSearchGiphys;
+});
+
